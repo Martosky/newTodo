58dd7ea9933372c667e9fc8ec895754ba1eee06b
Martosky <imoniaromartins@yahoo.com>
2025-03-28 11:21:05 +0100

    added the functionality

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const inputElem = document.getElementById("input")
 const button = document.getElementById("btn")
 const output = document.getElementById("output")
 
+button.addEventListener("click", ()=>{
+    if (inputElem.value === ""){
+        alert("Type in a valid plan")
+    }else{
+        const inputVal = inputElem.value;
+        const li = document.createElement("li");
+        li.innerText = inputVal;
+        output.appendChild(li);
+        const span = document.createElement("span");
+        span.innerText = "\u00d7"
+        li.appendChild(span)
+    }
+    inputElem.value = "";
+})
